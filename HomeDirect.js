cd8cfce798021bdce47cc89a37990f4515fa3977
LoversRhythm <56707694+LoversRhythm@users.noreply.github.com>
2026-03-02 11:08:21 +0800

    Add files via upload

---
+++ HomeDirect.js
@@ -0,0 +1,8 @@
+const HOME_SSID = "@MAINCHU_SS_5G";
+const currentSSID = $network.wifi.ssid;
+if (currentSSID === HOME_SSID) {
+    $surge.setOutboundMode("direct");
+} else {
+    $surge.setOutboundMode("rule");
+}
+$done();
